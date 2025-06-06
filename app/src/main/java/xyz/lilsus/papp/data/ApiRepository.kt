19ddf67035e113bf8be169ba9d1359b7d3046942
NicolaLS <sus.nym@proton.me>
2025-06-06 13:57:05 +0200

    remove unused code

--- app/src/main/java/xyz/lilsus/papp/data/ApiRepository.kt
+++ app/src/main/java/xyz/lilsus/papp/data/ApiRepository.kt
@@ -8,27 +8,6 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class ApiRepository {
-
-    suspend fun fetchDataFromApi(): String = withContext(Dispatchers.IO) {
-        val url = URL("https://jsonplaceholder.typicode.com/todos/1")
-        val connection = url.openConnection() as HttpURLConnection
-
-        try {
-            connection.requestMethod = "GET"
-            connection.connectTimeout = 5000
-            connection.readTimeout = 5000
-
-            val responseCode = connection.responseCode
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                connection.inputStream.bufferedReader().use { it.readText() }
-            } else {
-                "Error: $responseCode"
-            }
-        } finally {
-            connection.disconnect()
-        }
-    }
-
     // TODO: only attempt to pay ln invoices or bitcoin:xxx?ln=xxx
     suspend fun payInvoice(paymentRequest: String): String = withContext(Dispatchers.IO) {
         val url = URL("https://api.blink.sv/graphql")
