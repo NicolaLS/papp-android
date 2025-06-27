ef9f32cf721901689b3a0cf7434307f85dd74cce
NicolaLS <sus.nym@proton.me>
2025-06-27 17:02:00 -0600

    tests: Unit Test `PayInvoiceResponse.parse`

---
+++ app/src/test/java/xyz/lilsus/papp/data/repository/blink/dto/PayInvoiceResponseTest.kt
@@ -0,0 +1,138 @@
+package xyz.lilsus.papp.data.repository.blink.dto
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import xyz.lilsus.papp.domain.model.SendPaymentResult
+import kotlin.math.abs
+
+class PayInvoiceResponseTest {
+    @Test
+    fun `parse PayInvoiceResponse with missing status correctly`() {
+        val responseMissingStatus = PayInvoiceResponse(
+            PayInvoiceData(
+                PaymentSendPayload(
+                    errors = emptyList(),
+                    status = null,
+                    transaction = null
+                )
+            )
+        )
+        val sendPaymentResult = responseMissingStatus.parse()
+        assertTrue(sendPaymentResult is SendPaymentResult.Failure)
+    }
+
+    @Test
+    fun `parse PayInvoiceResponse with ALREADY_PAID status correctly`() {
+        val responseAlreadyPaidStatus = PayInvoiceResponse(
+            PayInvoiceData(
+                PaymentSendPayload(
+                    errors = emptyList(),
+                    status = PaymentSendResult.ALREADY_PAID,
+                    transaction = null
+                )
+            )
+        )
+        val sendPaymentResult = responseAlreadyPaidStatus.parse()
+        assertTrue(sendPaymentResult is SendPaymentResult.AlreadyPaid)
+    }
+
+    @Test
+    fun `parse PayInvoiceResponse with PENDING status correctly`() {
+        val responsePendingStatus = PayInvoiceResponse(
+            PayInvoiceData(
+                PaymentSendPayload(
+                    errors = emptyList(),
+                    status = PaymentSendResult.PENDING,
+                    transaction = null
+                )
+            )
+        )
+        val sendPaymentResult = responsePendingStatus.parse()
+        assertTrue(sendPaymentResult is SendPaymentResult.Pending)
+    }
+
+    @Test
+    fun `parse PayInvoiceResponse with FAILURE status without errors correctly`() {
+        val responseFailureStatusWithoutErrors = PayInvoiceResponse(
+            PayInvoiceData(
+                PaymentSendPayload(
+                    errors = emptyList(),
+                    status = PaymentSendResult.FAILURE,
+                    transaction = null
+                )
+            )
+        )
+
+        var sendPaymentResult = responseFailureStatusWithoutErrors.parse()
+        assertTrue(sendPaymentResult is SendPaymentResult.Failure)
+    }
+
+    @Test
+    fun `parse PayInvoiceResponse with FAILURE status with errors correctly`() {
+        val responseFailureStatusWithErrors = PayInvoiceResponse(
+            PayInvoiceData(
+                PaymentSendPayload(
+                    errors = listOf(
+                        xyz.lilsus.papp.data.repository.blink.dto.Error(
+                            "69420",
+                            "error message",
+                            listOf("path0", "path1")
+                        )
+                    ),
+                    status = PaymentSendResult.FAILURE,
+                    transaction = null
+                )
+            )
+        )
+
+        val sendPaymentResult = responseFailureStatusWithErrors.parse()
+        assertTrue(sendPaymentResult is SendPaymentResult.Failure)
+    }
+
+    @Test
+    fun `parse PayInvoiceResponse with SUCCESS status but no transaction correctly`() {
+        val responseSuccessStatusNoTransaction = PayInvoiceResponse(
+            PayInvoiceData(
+                PaymentSendPayload(
+                    errors = emptyList(),
+                    status = PaymentSendResult.SUCCESS,
+                    transaction = null
+                )
+            )
+        )
+        val sendPaymentResult = responseSuccessStatusNoTransaction.parse()
+        assertTrue(sendPaymentResult is SendPaymentResult.Failure)
+    }
+
+    @Test
+    fun `parse PayInvoiceResponse with SUCCESS status correctly`() {
+        val transaction = Transaction(
+            direction = TxDirection.SEND,
+            memo = null,
+            settlementAmount = 420,
+            settlementFee = 69,
+            status = TxStatus.SUCCESS
+        )
+        val responseSuccessStatus = PayInvoiceResponse(
+            PayInvoiceData(
+                PaymentSendPayload(
+                    errors = emptyList(),
+                    status = PaymentSendResult.SUCCESS,
+                    transaction = transaction
+                )
+            )
+        )
+        val sendPaymentResult = responseSuccessStatus.parse()
+        assertTrue(sendPaymentResult is SendPaymentResult.Success)
+        val data = (sendPaymentResult as SendPaymentResult.Success).data
+        val amountPaidTotal = abs(transaction.settlementAmount) // 420
+        val feePaid = abs(transaction.settlementFee) // 69
+        val amountPaid = amountPaidTotal - feePaid // 351
+
+        assertEquals(amountPaid, data.amountPaid)
+        assertEquals(feePaid, data.feePaid)
+        assertNull(data.memo)
+    }
+}
