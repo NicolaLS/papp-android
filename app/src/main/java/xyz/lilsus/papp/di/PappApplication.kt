feb1e6a15d0eb30ebf50306694a01d3d478a90b5
NicolaLS <sus.nym@proton.me>
2025-08-21 08:53:47 -0600

    app: Limit application to use default back camera
    By limiting the application to use the default back camera we reduce
    startup latency. (See: CameraX Configuration, Camera Limiter).

--- app/src/main/java/xyz/lilsus/papp/di/PappApplication.kt
+++ app/src/main/java/xyz/lilsus/papp/di/PappApplication.kt
@@ -1,14 +1,24 @@
 package xyz.lilsus.papp.di
 
 import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraXConfig
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
-class PappApplication : Application() {
+class PappApplication : Application(), CameraXConfig.Provider {
 
     private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     val appDependencies by lazy {
         AppDependencies(applicationContext, applicationScope)
+    }
+
+    override fun getCameraXConfig(): CameraXConfig {
+        return CameraXConfig.Builder.fromConfig(Camera2Config.defaultConfig())
+            // Reduce startup latency for the cameras the application uses.
+            .setAvailableCamerasLimiter(CameraSelector.DEFAULT_BACK_CAMERA)
+            .build()
     }
 }
