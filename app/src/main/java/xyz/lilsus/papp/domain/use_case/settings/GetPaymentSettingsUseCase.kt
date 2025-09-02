567f9e5e24568f508f1a5a95780e9e45d50f1e45
NicolaLS <sus.nym@proton.me>
2025-09-01 18:40:47 -0600

    domain: Simplify `GetPaymentSettingsUseCase`
    I am pretty lost on this whole design pattern and the first example I
    saw used flows and a `Ressource` type but it does not really make sense
    here.

--- app/src/main/java/xyz/lilsus/papp/domain/use_case/settings/GetPaymentSettingsUseCase.kt
+++ app/src/main/java/xyz/lilsus/papp/domain/use_case/settings/GetPaymentSettingsUseCase.kt
@@ -1,28 +1,12 @@
 package xyz.lilsus.papp.domain.use_case.settings
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import xyz.lilsus.papp.common.Resource
+import kotlinx.coroutines.flow.first
 import xyz.lilsus.papp.domain.model.settings.PaymentSettings
 import xyz.lilsus.papp.domain.repository.SettingsRepository
 
 class GetPaymentSettingsUseCase(private val settingsRepository: SettingsRepository) {
-    operator fun invoke(): Flow<Resource<PaymentSettings>> =
-        flow {
-            emit(Resource.Loading())
-            settingsRepository.paymentSettings.collect { settings ->
-                emit(
-                    Resource.Success(
-                        PaymentSettings(
-                            settings.alwaysConfirmPayment,
-                            settings.confirmPaymentAbove,
-                        )
-                    )
-                )
-            }
-        }.catch { e ->
-            emit(Resource.Error(e.message ?: "Unknown Error"))
-        }
+    suspend operator fun invoke(): PaymentSettings {
+        val paymentSettingsModel = settingsRepository.paymentSettings.first()
+        return paymentSettingsModel
+    }
 }
-
